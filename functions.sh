4c4aa0fb5dc7f7242c4d3f169dff8b9e51153e6a
Justin Hyland <j@linux.com>
2017-01-24 13:26:49 -0700

    first commit

---
+++ functions.sh
@@ -0,0 +1,308 @@
+
+# Trim lines from string
+#
+# Examples
+#   $ echo "    Hello    World   "
+#       Hello    World
+#   $ echo "    Hello    World   " | trim
+#   Hello    World
+#   $ trim "$(echo '    Hello    World   ')"
+#   Hello    World
+function trim {
+  if [ $# -eq 0 ]
+  then
+    data=$(cat)
+  else
+    data="$*"
+  fi
+
+  data="${data#"${data%%[![:space:]]*}"}"
+  data="${data%"${data##*[![:space:]]}"}"
+
+  echo -n "$data"
+}
+
+# Right trim
+function rtrim {
+  if [ $# -eq 0 ]
+  then
+    data=$(cat)
+  else
+    data="$*"
+  fi
+
+  data="${data%"${data##*[![:space:]]}"}"
+
+  echo -n "$data"
+}
+
+# Left trim
+function ltrim {
+  if [ $# -eq 0 ]
+  then
+    data=$(cat)
+  else
+    data="$*"
+  fi
+
+  data="${data#"${data%%[![:space:]]*}"}"
+
+  echo -n "$data"
+}
+
+# Strip all empty lines and comments
+#
+# Examples
+#   $ echo -e "foo\n#bar\n\nbaz\n#bang\n\t# idk"
+#   foo
+#   #bar
+# 
+#   baz
+#   #bang
+#     # idk
+#   $ echo -e "foo\n#bar\n\nbaz\n#bang\n\t# idk" | realdata
+#   foo
+#   baz
+#   $ realdata "$(echo -e 'foo\n#bar\n\nbaz\n#bang\n\t# idk')"
+#   foo
+#   baz
+function realdata {
+  if [ $# -eq 0 ]
+  then
+    data=$(cat)
+  else
+    data="$*"
+  fi
+
+  echo "$data" | grep -Ev '^($|\s*#)'
+}
+
+
+# Line Count
+#   $ echo -e "foo\n#bar\n\nbaz\n#bang\n\t# idk"
+#   foo
+#   #bar
+#   
+#   baz
+#     #bang
+#   $ echo -e "foo\n#bar\n\nbaz\n#bang\n\t# idk" | wc -l
+#        6
+#   $ echo -e "foo\n#bar\n\nbaz\n#bang\n\t# idk" | lc
+#   6
+function lc {
+  if [ $# -eq 0 ]
+  then
+    data=$(cat)
+  else
+    data="$*"
+  fi
+
+  echo "$data" | wc -l | trim
+}
+
+# REAL Line Count
+#
+#   $ echo -e "foo\n#bar\n\nbaz\n#bang\n\t# idk"
+#   foo
+#   #bar
+#   
+#   baz
+#     #bang
+#   $ echo -e "foo\n#bar\n\nbaz\n#bang\n\t# idk" | wc -l
+#        6
+#   $ echo -e "foo\n#bar\n\nbaz\n#bang\n\t# idk" | lc
+#   6
+#   $ echo -e "foo\n#bar\n\nbaz\n#bang\n\t# idk" | realdata | wc -l
+#        2
+#   $ echo -e "foo\n#bar\n\nbaz\n#bang\n\t# idk" | rlc
+#   2
+function rlc {
+  if [ $# -eq 0 ]
+  then
+    data=$(cat)
+  else
+    data="$*"
+  fi
+
+  echo "$data" | realdata |  wc -l | trim
+}
+
+# Upper case alphabetical characters in string
+function upper {
+  if [ $# -eq 0 ]
+  then
+    data=$(cat)
+  else
+    data="$*"
+  fi
+
+  echo "$data" | tr '[a-z]' '[A-Z]'
+}
+
+# Lower case alphabetical characters in string
+function lower {
+  if [ $# -eq 0 ]
+  then
+    data=$(cat)
+  else
+    data="$*"
+  fi
+
+  echo "$data" | tr '[A-Z]' '[a-z]'
+}
+
+# Redirect specified content to STDERR 
+function tostderr {
+  if [ $# -eq 0 ]
+  then
+    data=$(cat)
+  else
+    data="$*"
+  fi
+
+  echo "${data}" 1>&2
+}
+
+# Redirect specified content to STDOUT
+function tostdout {
+  if [ $# -eq 0 ]
+  then
+    data=$(cat)
+  else
+    data="$*"
+  fi
+
+  echo "${data}" 2>&1
+}
+
+# Verify that a binary exists and is in the $PATH
+function checkbin {
+  which "${1}" &>/dev/null
+}
+
+# Get the date in a specific format
+#
+# Examples
+#   $ getdate epoch
+#   1485287542
+#   $ getdate time
+#   12:52:25 PM
+#   $ getdate time12
+#   12:52:26 PM
+#   $ getdate time24
+#   12:52:31
+#   $ getdate day
+#   2017-01-24
+#   $ getdate daytime
+#   2017-01-24 12:52:37 PM
+#   $ getdate daytime24
+#   2017-01-24 12:52:39
+#   $ getdate daytime12
+#   2017-01-24 12:52:44 PM
+#   $ getdate timeday
+#   12:52:49 PM 2017-01-24
+#   $ getdate time12day
+#   12:52:52 PM 2017-01-24
+#   $ getdate time24day
+#   12:52:54 2017-01-24
+function getdate {
+  if [[ $# -eq 0 ]]
+  then
+    req="daytime24"
+  else
+    req="$(lower $1)"
+  fi
+
+  fmtDay="%Y-%m-%d"
+  fmtTime24="%H:%M:%S"
+  fmtTime12="%I:%M:%S %p"
+  fmtEpoch="%a %b %d %T %Z %Y"
+
+  #date "+%Y-%m-%d %H:%M:%S"
+
+  if [[ $req == 'epoch' ]]
+  then
+    date -j -f "${fmtEpoch}" "$(date)" "+%s"
+  elif [[ $req == 'time' ]] || [[ $req == 'time12' ]]
+  then
+    date "+${fmtTime12}"
+  elif [[ $req == 'time24' ]]
+  then
+    date "+${fmtTime24}"
+  elif [[ $req == 'day' ]]
+  then
+    date "+${fmtDay}"
+  elif [[ $req == 'daytime' ]] || [[ $req == 'daytime12' ]]
+  then
+    date "+${fmtDay} ${fmtTime12}"
+  elif [[ $req == 'daytime24' ]]
+  then
+    date "+${fmtDay} ${fmtTime24}"
+  elif [[ $req == 'timeday' ]] || [[ $req == 'time12day' ]]
+  then
+    date "+${fmtTime12} ${fmtDay}"
+  elif [[ $req == 'time24day' ]]
+  then
+    date "+${fmtTime24} ${fmtDay}"
+  else
+    tostderr "Invalid or illegal date format '${req}'"
+    date "+${fmtTime24} ${fmtDay}"
+  fi
+}
+
+function inarray {
+  local e
+
+  for e in "${@:2}"
+  do 
+    [[ "$e" == "$1" ]] && return 0
+  done
+
+  return 1
+}
+
+# date "+%m%d%H%M%Y.%S"
+
+function diaglog {
+  levels=("DEBUG" "GENERAL" "INFO" "WARN" "ERROR")  
+
+  function istype {
+    inarray "${1}" "${levels[@]}"
+  }
+
+  if [[ $# -eq 0 ]]
+  then
+    tostderr "the diaglog function was called with no parameters"
+    return
+  fi
+
+  loglvldef="GENERAL"
+  loglvl="${loglvldef}"
+  logto="${generalLog}"
+
+  # If only ONE arguments was provided..
+  if [[ $# -eq 1 ]]
+  then
+    data="${1}"
+
+  # If TWO (or more) arguments were provided..
+  elif [[ $# -gt 1 ]]
+  then
+
+    if [[ $(istype "$(upper ${1})"; echo $?) -eq 0 ]]
+    then
+      loglvl="$(upper ${1})"
+      data="${2}"
+    elif [[ $(istype "$(upper ${2})"; echo $?) -eq 0 ]]
+    then
+      loglvl="$(upper ${2})"
+      data="${1}"
+    else
+      data="${1}"
+      loglvl="${2}"
+    fi
+    logto='./test'
+  fi
+
+  printf "%-10s | %-19s | %-7s | %s\n" "$(getdate epoch)" "$(getdate daytime24)" "${loglvl}" "${data}"  >> "${logto}"
+}
---
+++ get-diagnostics.sh
@@ -0,0 +1,87 @@
+#ps aux
+#netstat (all TCP connections), 
+#lsof
+#mounts
+#
+#proc folder
+#
+#
+#mkdir -vp ~/diagnostics/{system/{stats,processes,files,profile},network}
+#
+#find /System -maxdepth 3
+#
+#sysctl -a hw
+#hw
+#kern
+#machdep
+#net
+#vm
+#user
+#debug
+#
+#top -u  -l 1 > file
+
+. ./functions.sh
+
+clear 
+
+outdir='./diagnostics'
+runtimestamp=$(getdate epoch)
+generalLog="./diagnostics-${runtimestamp}.log"
+
+mkdir -vp $outdir/{system/{stats,processes,files,profile},network}
+
+# Crate the general diagnostics outlog log
+diaglog "Diagnostics started by the user $(whoami)"
+#echo "Diagnostics started at $(date) by the user $(whoami)" > "${generalLog}"
+
+checkbin system_profiler
+
+if [[ $? == 0 ]]
+then
+  # ----------------------------------------
+  # SYSTEM PROFILER
+  spDataTypeList="$(system_profiler -listDataTypes | sed 1d)"
+  spDataTypeCount=$(rlc "${spDataTypeList}")
+
+  echo -e "Getting System Profile Data Content (${spDataTypeCount} types)\n"
+
+  sysprofileList="system_profiler.list"
+  printformat="%-30s %-30s\n"
+
+  # Create the system_profile output text file
+  date > "${sysprofileList}"
+  echo '' >> "${sysprofileList}"
+  printf "${printformat}" "DATATYPE" "LINES" >> "${sysprofileList}"
+  printf "${printformat}" "--------------------------" "------" >> "${sysprofileList}"
+
+  # Header output for the data going to STDOUT
+  printf "\t%-10s %-30s %-10s\n" "STATUS" "DATA TYPE" "RESULT" 
+  printf "\t%-10s %-30s %-10s\n" "-------" "----------" "-------" 
+
+  i=0
+  echo "${spDataTypeList}"| while read dataType
+  do
+    i=$((i+1))
+    outFile="${outdir}/system/profile/$dataType.txt"
+
+    printf "\t%-10s %-30s" "[$i/$spDataTypeCount]" "${dataType}"
+
+    system_profiler -detailLevel full $dataType > "${outFile}"
+
+    res=$?
+
+    if [[ $res != 0 ]]
+    then
+      printf " %-10s %-30s\n" "Failed" "${res}"
+    else
+      lines=$(cat "${outFile}" | rlc)
+      disp=$([[ $lines == 0 ]] && echo "-Empty-" || echo "${lines} lines")
+      printf " %-10s %-30s\n" "Success" "${disp}"
+
+      printf "${printformat}" "${dataType}" "${lines}" >> "${sysprofileList}"
+    fi
+  done
+else
+  echo "No 'system_profiler' binary found in PATH ${PATH}" >>  "${generalLog}"
+fi
